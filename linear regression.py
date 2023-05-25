6a0f7567eeb6e6ded523a2b74f6b413d38ad5c20
Matheus Souza <99676439+SouzaMatheus1@users.noreply.github.com>
2023-05-24 21:47:37 -0300

    gitignore

--- linear regression.py
+++ linear regression.py
@@ -7,29 +7,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
+# Definindo DataFrame
 df = pd.read_csv("penguins_raw.csv")
 
+# Drop valores com zero
 df.drop('Unnamed: 0', axis=1, inplace=True)
 
+# Definindo DataFrame formatado
 df2 = pd.DataFrame()
 df2['Culmen Length (mm)']  = df['Culmen Length (mm)']
 df2['Culmen Depth (mm)']  = df['Culmen Depth (mm)']
-df2['Flipper Length (mm)']  = df['Flipper Length (mm)']
 df2['Body Mass (g)']  =  df['Body Mass (g)']
+df2['Flipper Length (mm)']  = df['Flipper Length (mm)']
+# Drop de valores NaN
 df2.dropna(inplace=True)
 
-print(df2)
+# Definindo y e x
+y = df2['Flipper Length (mm)']
+X = df2.drop(columns=['Flipper Length (mm)'])
 
-X = df2[0]#,df2[1],df2[2]
-y = df2[3]
+# Treinando algoritmo
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
-# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-
+# Testando valores
 from sklearn.linear_model import LinearRegression
-# linreg = LinearRegression()
-# linreg.fit(X_train, y_train)
-print('sucesso')
-# l1 = linreg.score(X_train, y_train)
-# l2 = linreg.score(X_test, y_test)
-# print("R² of Linear Regression on training set: {:.3f}".format(l1))
-# print("R² of Linear Regression on test set: {:.3f}".format(l2))
+linreg = LinearRegression()
+linreg.fit(X_train, y_train)
+# print('sucesso')
+l1 = linreg.score(X_train, y_train)
+l2 = linreg.score(X_test, y_test)
+print("R² de Linear Regression de teste: {:.3f}".format(l1))
+print("R² de Linear Regression de treino: {:.3f}".format(l2))
